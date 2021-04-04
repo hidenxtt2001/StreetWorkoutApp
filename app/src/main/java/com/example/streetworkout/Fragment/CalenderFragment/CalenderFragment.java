b95069492f3b524c6b908a5f4970a6e5db23a626
Hưng Nguyễn Xuân <19521578@ms.uit.edu.vn>
2021-04-04 21:56:54 +0700

    test

--- app/src/main/java/com/example/streetworkout/Fragment/CalenderFragment/CalenderFragment.java
+++ app/src/main/java/com/example/streetworkout/Fragment/CalenderFragment/CalenderFragment.java
@@ -23,7 +23,6 @@ public class CalenderFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
