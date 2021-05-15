00f3528aaf7fe326a57dbcb42778d7a835961f4a
Nguyễn Thành Hiếu <19521505@gm.uit.edu.vn>
2021-05-15 11:06:48 +0700

    testCalendar

--- app/src/main/java/com/example/streetworkout/Fragment/CalenderFragment/CalenderFragment.java
+++ app/src/main/java/com/example/streetworkout/Fragment/CalenderFragment/CalenderFragment.java
@@ -3,12 +3,21 @@ package com.example.streetworkout.Fragment.CalenderFragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toolbar;
 
 import com.example.streetworkout.R;
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class CalenderFragment extends Fragment {
@@ -18,17 +27,60 @@ public class CalenderFragment extends Fragment {
         // Required empty public constructor
     }
 
-
-
+    private  View root;
+    private AppBarLayout appBarLayout;
+    private CollapsingToolbarLayout collapsingToolbarLayout;
+    private Toolbar toolbar;
+    private RecyclerView recyclerViewExercise;
+    private ExercisesEachDayAdapter exercisesEachDayAdapter;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.main_fragment_calender, container, false);
+        root = inflater.inflate(R.layout.main_fragment_calender, container, false);
+        //initUi
+        initUi();
+        initRecyclerView();
+        return root;
     }
+
+    private void initUi(){
+        appBarLayout = root.findViewById(R.id.appBar);
+        collapsingToolbarLayout = root.findViewById(R.id.collapsingToolbarLayout);
+        toolbar = root.findViewById(R.id.toolbar);
+        recyclerViewExercise = root.findViewById(R.id.recycler_exercises);
+    }
+
+    private void initRecyclerView(){
+        recyclerViewExercise.setHasFixedSize(true);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(root.getContext());
+        recyclerViewExercise.setLayoutManager(linearLayoutManager);
+
+        exercisesEachDayAdapter = new ExercisesEachDayAdapter();
+        exercisesEachDayAdapter.setData(getListExercises());
+
+        recyclerViewExercise.setAdapter(exercisesEachDayAdapter);
+
+    }
+
+    private List<ExercisesEachDay> getListExercises(){
+        List<ExercisesEachDay> list = new ArrayList<>();
+        list.add(new ExercisesEachDay(R.drawable.calendar_image_dayone, "Workouts", "Chest"));
+        list.add(new ExercisesEachDay(R.drawable.calendar_image_dayone, "Workouts", "Chest"));
+        list.add(new ExercisesEachDay(R.drawable.calendar_image_dayone, "Workouts", "Chest"));
+        list.add(new ExercisesEachDay(R.drawable.calendar_image_dayone, "Workouts", "Chest"));
+        list.add(new ExercisesEachDay(R.drawable.calendar_image_dayone, "Workouts", "Chest"));
+        list.add(new ExercisesEachDay(R.drawable.calendar_image_dayone, "Workouts", "Chest"));
+        list.add(new ExercisesEachDay(R.drawable.calendar_image_dayone, "Workouts", "Chest"));
+        list.add(new ExercisesEachDay(R.drawable.calendar_image_dayone, "Workouts", "Chest"));
+        return list;
+    }
+
 }
---
+++ app/src/main/java/com/example/streetworkout/Fragment/CalenderFragment/ExercisesEachDayAdapter.java
@@ -0,0 +1,64 @@
+package com.example.streetworkout.Fragment.CalenderFragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.streetworkout.R;
+
+import java.util.List;
+
+public class ExercisesEachDayAdapter extends RecyclerView.Adapter<ExercisesEachDayAdapter.ExercisesViewHolder> {
+
+    private List<ExercisesEachDay> mListExercises;
+
+    public void setData(List<ExercisesEachDay> list) {
+        this.mListExercises = list;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public ExercisesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view;
+        view = LayoutInflater.from(parent.getContext()).inflate(R.layout.calendar_item_day_exercise, parent, false);
+        return new ExercisesViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ExercisesEachDayAdapter.ExercisesViewHolder holder, int position) {
+        ExercisesEachDay exercisesEachDay = mListExercises.get(position);
+        if(exercisesEachDay == null){
+            return;
+        }
+        holder.imageDay.setImageResource(exercisesEachDay.getResourceImg());
+        holder.txtDescribe.setText(exercisesEachDay.getNameDescribe());
+        holder.txtExercise.setText(exercisesEachDay.getNameExercise());
+    }
+
+    @Override
+    public int getItemCount() {
+        if(mListExercises != null){
+            return mListExercises.size();
+        }
+        return 0;
+    }
+
+    public class ExercisesViewHolder extends RecyclerView.ViewHolder{
+        private ImageView imageDay;
+        private TextView txtDescribe;
+        private TextView txtExercise;
+
+        public ExercisesViewHolder(@NonNull View itemView){
+            super(itemView);
+            imageDay = itemView.findViewById(R.id.img_day);
+            txtDescribe = itemView.findViewById(R.id.name_describe);
+            txtExercise = itemView.findViewById(R.id.name_exercise);
+        }
+    }
+}
