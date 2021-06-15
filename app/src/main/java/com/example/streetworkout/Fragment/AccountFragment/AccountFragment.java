4d693a25d23c419a1d28ee6c9c80b76df97f1cc2
Long <longdh210@gmail.com>
2021-06-15 21:30:45 +0700

    NewsFeed

--- app/src/main/java/com/example/streetworkout/Fragment/AccountFragment/AccountFragment.java
+++ app/src/main/java/com/example/streetworkout/Fragment/AccountFragment/AccountFragment.java
@@ -156,7 +156,7 @@ public class AccountFragment extends Fragment {
                 k.setUserInfor(userInfor);
                 listStatus.add(k);
                 accountFragmentAdapter.notifyDataSetChanged();
-
+                
 
             }
 
--- app/src/main/java/com/example/streetworkout/Fragment/NewsFeedFragment/NewsFeedFragment.java
+++ app/src/main/java/com/example/streetworkout/Fragment/NewsFeedFragment/NewsFeedFragment.java
@@ -2,17 +2,37 @@ package com.example.streetworkout.Fragment.NewsFeedFragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.streetworkout.Fragment.AccountFragment.AccountFragment;
+import com.example.streetworkout.Fragment.AccountFragment.AccountFragmentAdapter;
 import com.example.streetworkout.R;
+import com.example.streetworkout.StatusWorkout.StatusWorkout;
+import com.example.streetworkout.User.UserInfor;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 
 public class NewsFeedFragment extends Fragment {
 
+    View root;
+    public static UserInfor userInfor;
+    RecyclerView recyclerView;
+    ArrayList<StatusWorkout> listStatus;
+    AccountFragmentAdapter accountFragmentAdapter;
 
     public NewsFeedFragment() {
         // Required empty public constructor
@@ -29,6 +49,69 @@ public class NewsFeedFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.main_fragment_newsfeed, container, false);
+        root = inflater.inflate(R.layout.main_fragment_newsfeed, container, false);
+
+        SetUpInfor();
+
+        return root;
+
+    }
+
+    private void SetUpInfor() {
+
+        recyclerView = root.findViewById(R.id.recyclerViewNewsFeed);
+
+        listStatus = new ArrayList<>();
+
+        SetStatus();
+
+        accountFragmentAdapter = new AccountFragmentAdapter(NewsFeedFragment.this.getContext(),listStatus);
+        recyclerView.setAdapter(accountFragmentAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(NewsFeedFragment.this.getContext()));
+    }
+
+    private void SetStatus() {
+
+        FirebaseDatabase.getInstance().getReference().child("StatusUserExercise").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                StatusWorkout k = snapshot.getValue(StatusWorkout.class);
+                FirebaseDatabase.getInstance().getReference().child("UserInfos").child(k.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull  DataSnapshot snapshot) {
+                        k.setUserInfor(snapshot.getValue(UserInfor.class));
+                        listStatus.add(k);
+                        accountFragmentAdapter.notifyDataSetChanged();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull  DatabaseError error) {
+
+                    }
+                });
+
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable  String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull  DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull  DataSnapshot snapshot, @Nullable  String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
