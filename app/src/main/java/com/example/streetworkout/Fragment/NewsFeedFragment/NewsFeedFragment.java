6213998016b36d09a4be615359801c2766b29c81
Nguyen Xuan Hung <nxhung16102001@gmail.com>
2021-06-24 15:43:08 +0700

    card

--- app/src/main/java/com/example/streetworkout/Fragment/NewsFeedFragment/NewsFeedFragment.java
+++ app/src/main/java/com/example/streetworkout/Fragment/NewsFeedFragment/NewsFeedFragment.java
@@ -1,5 +1,6 @@
 package com.example.streetworkout.Fragment.NewsFeedFragment;
 
+import android.graphics.Path;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -24,7 +25,14 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
 
 
 public class NewsFeedFragment extends Fragment {
@@ -73,46 +81,87 @@ public class NewsFeedFragment extends Fragment {
 
     private void SetStatus() {
 
-        FirebaseDatabase.getInstance().getReference().child("StatusUserExercise").addChildEventListener(new ChildEventListener() {
+//        FirebaseDatabase.getInstance().getReference().child("StatusUserExercise").addChildEventListener(new ChildEventListener() {
+//            @Override
+//            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+//
+//                StatusWorkout k = snapshot.getValue(StatusWorkout.class);
+//                if(k.getUid().equals(MainActivity.userInfor.getUid())) return;
+//                FirebaseDatabase.getInstance().getReference().child("UserInfos").child(k.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+//                    @Override
+//                    public void onDataChange(@NonNull  DataSnapshot snapshot) {
+//                        k.setUserInfor(snapshot.getValue(UserInfor.class));
+//                        listStatus.add(k);
+//                        accountFragmentAdapter.notifyDataSetChanged();
+//                    }
+//
+//                    @Override
+//                    public void onCancelled(@NonNull  DatabaseError error) {
+//
+//                    }
+//                });
+//
+//
+//            }
+//
+//            @Override
+//            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable  String previousChildName) {
+//
+//            }
+//
+//            @Override
+//            public void onChildRemoved(@NonNull  DataSnapshot snapshot) {
+//
+//            }
+//
+//            @Override
+//            public void onChildMoved(@NonNull  DataSnapshot snapshot, @Nullable  String previousChildName) {
+//
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//        });
+
+        FirebaseDatabase.getInstance().getReference().child("StatusUserExercise").addValueEventListener(new ValueEventListener() {
             @Override
-            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                listStatus.clear();
+                for ( DataSnapshot i : snapshot.getChildren()) {
 
-                StatusWorkout k = snapshot.getValue(StatusWorkout.class);
-                if(k.getUid().equals(MainActivity.userInfor.getUid())) return;
-                FirebaseDatabase.getInstance().getReference().child("UserInfos").child(k.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull  DataSnapshot snapshot) {
-                        k.setUserInfor(snapshot.getValue(UserInfor.class));
-                        listStatus.add(k);
-                        accountFragmentAdapter.notifyDataSetChanged();
-                    }
+                    StatusWorkout k = i.getValue(StatusWorkout.class);
+                    if(k.getUid().equals(MainActivity.userInfor.getUid())) continue;
+                    FirebaseDatabase.getInstance().getReference().child("UserInfos").child(k.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull  DataSnapshot snapshot) {
+                            SimpleDateFormat parser = new SimpleDateFormat("dd/MM/yyy");
+                            k.setUserInfor(snapshot.getValue(UserInfor.class));
+                            listStatus.add(k);
+                            Collections.sort(listStatus, new Comparator<StatusWorkout>() {
+                                public int compare(StatusWorkout o1, StatusWorkout o2) {
+                                    try {
+                                        return parser.parse(o2.getDateComplate()).compareTo(parser.parse(o1.getDateComplate()));
+                                    } catch (ParseException e) {
+                                        e.printStackTrace();
+                                    }
+                                    return 0;
+                                }
+                            });
+                            accountFragmentAdapter.notifyDataSetChanged();
+                        }
 
-                    @Override
-                    public void onCancelled(@NonNull  DatabaseError error) {
+                        @Override
+                        public void onCancelled(@NonNull  DatabaseError error) {
 
-                    }
-                });
-
-
+                        }
+                    });
+                }
             }
 
             @Override
-            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable  String previousChildName) {
-
-            }
-
-            @Override
-            public void onChildRemoved(@NonNull  DataSnapshot snapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(@NonNull  DataSnapshot snapshot, @Nullable  String previousChildName) {
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
+            public void onCancelled(@NonNull @NotNull DatabaseError error) {
 
             }
         });
